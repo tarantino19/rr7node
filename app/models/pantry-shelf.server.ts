302fbd48a59c470c1623d7b1ad46bf263f364acf
tarantino19 <tolentinored19@gmail.com>
2024-12-02 10:46:43 +0800

    update pantry query

--- app/models/pantry-shelf.server.ts
+++ app/models/pantry-shelf.server.ts
@@ -2,7 +2,15 @@ import db from '~/db.server';
 
 export function getAllShelves() {
 	try {
-		return db.pantryShelf.findMany();
+		return db.pantryShelf.findMany({
+			include: {
+				items: {
+					orderBy: {
+						name: 'asc',
+					},
+				},
+			},
+		});
 	} catch (error) {
 		console.error('Error fetching shelves:', error);
 		throw new Error('Failed to fetch shelves');
--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,38 +1,38 @@
 import { LoaderFunction, useLoaderData } from 'react-router';
 import { PantryShelf, PantryItem } from '@prisma/client';
 import { getAllShelves } from '~/models/pantry-shelf.server';
-import { getAllPantryItems } from '~/models/pantry-item.server';
 
 export const loader: LoaderFunction = async () => {
-	const [shelves, items] = await Promise.all([getAllShelves(), getAllPantryItems()]);
-	return new Response(JSON.stringify({ shelves, items }), {
+	const shelves = await getAllShelves();
+	return new Response(JSON.stringify({ shelves }), {
 		headers: { 'Content-Type': 'application/json' },
 	});
 };
 
 export default function Pantry() {
-	const { shelves, items } = useLoaderData() as { shelves: PantryShelf[]; items: PantryItem[] };
+	const { shelves } = useLoaderData<{ shelves: PantryShelf[] }>();
 
 	return (
 		<>
 			<div>
-				<h1 className='font-bold'>Pantry Shelves List</h1>
-				<div>
+				<ul className='flex gap-8 overflow-x-auto snap-x snap-mandatory'>
 					{shelves.map((shelf: PantryShelf) => (
-						<div key={shelf.id}>
-							<p>{shelf.name}</p>
-						</div>
+						<li
+							className='border-2 border-primary rounded-md p-4 w-[calc(100vw-2rem)] flex-none snap-center h-fit md:w-96'
+							key={shelf.id}
+						>
+							<h1 className='text-2xl font-extrabold mb-2'>{shelf.name}</h1>
+							<ul>
+								{/* @ts-ignore */}
+								{shelf.items.map((item: PantryItem) => (
+									<li className='py-2' key={item.id}>
+										{item.name}
+									</li>
+								))}
+							</ul>
+						</li>
 					))}
-				</div>
-				{''}
-				<h1 className='font-bold'>Pantry Items List</h1>
-				<div>
-					{items.map((item: PantryItem) => (
-						<div key={item.id}>
-							<p>{item.name}</p>
-						</div>
-					))}
-				</div>
+				</ul>
 			</div>
 		</>
 	);
